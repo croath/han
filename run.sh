cde6403ad6bede85c21cc4ab07890cb4c3a699f0
croath <croathliu@gmail.com>
2017-08-28 16:34:42 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-nohup python cnn.py --data_dir=/home/croath/Documents/training_data_64/positive_data/ --graph_dir=/home/croath/Documents/graphs/ --checkpoint_dir=/home/croath/Documents/checkpoints/ --charater_num=8877 > output.txt 2>&1&
+mkdir -p /mnt/liuzhenfu/graphs/
+mkdir -p /mnt/liuzhenfu/checkpoints/
+
+nohup python cnn.py --data_dir=/home/liuzhenfu/training_data/positive_data/ \
+ --valid_dir=/home/liuzhenfu/training_data/valid_data
+ --graph_dir=/mnt/liuzhenfu/graphs/ \
+ --checkpoint_dir=/mnt/liuzhenfu/checkpoints/ \
+ --charater_num=8877 \
+ --epoch_num=10 \
+ --batch_size=400 \
+ > output.txt 2>&1&
