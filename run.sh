6d92a063651274abcfb6ff07a07c1b2eda4006a4
croath <croathliu@gmail.com>
2017-08-28 16:38:41 +0800

    add \

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ mkdir -p /mnt/liuzhenfu/graphs/
 mkdir -p /mnt/liuzhenfu/checkpoints/
 
 nohup python cnn.py --data_dir=/home/liuzhenfu/training_data/positive_data/ \
- --valid_dir=/home/liuzhenfu/training_data/valid_data
+ --valid_dir=/home/liuzhenfu/training_data/valid_data \
  --graph_dir=/mnt/liuzhenfu/graphs/ \
  --checkpoint_dir=/mnt/liuzhenfu/checkpoints/ \
  --charater_num=8877 \
