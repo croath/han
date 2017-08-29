0fb3325035be2bad9bf3a2cb134d8fad261be7bd
croath <croathliu@gmail.com>
2017-08-29 10:50:34 +0800

    argmax instead of index 1

--- cnn.py
+++ cnn.py
@@ -134,10 +134,8 @@ def main(_):
                   label = labels_val[i]
                   logit = logits_val[i]
 
-                  log(label.tolist().index(1))
-
-                  input_char = int_to_chinese(label.tolist().index(1))
-                  output_char = int_to_chinese(logit.tolist().index(1))
+                  input_char = int_to_chinese(label.argmax())
+                  output_char = int_to_chinese(logit.argmax())
 
                   match = True if input_char == output_char else False
                   log('Input: %s\tOuput: %s\t%r' %(input_char, output_char, match))
