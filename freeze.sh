7c5dd1237a65a8f646d1e232d2b536a46ad6a278
croath <croathliu@gmail.com>
2017-09-07 09:36:46 +0800

    update freeze script

--- freeze.sh
+++ freeze.sh
@@ -1,3 +1,6 @@
-python3 graph_saver.py --model_dir=/mnt/liuzhenfu/models/ \
- --checkpoint_dir=/mnt/liuzhenfu/checkpoints/ \
+mkdir -p /mnt/liuzhenfu/models_new/
+
+python3 graph_saver.py --model_dir=/mnt/liuzhenfu/models_new/ \
+ --checkpoint_dir=/mnt/liuzhenfu/checkpoints_new/ \
  --gpu_fraction=0.5 \
+ > freeze.txt 2>&1&
