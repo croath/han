6d5c7ec0447d114b7c679556dae9b4a626b6da41
croath <croathliu@gmail.com>
2017-09-07 11:05:58 +0800

    modify valid

--- valid.sh
+++ valid.sh
@@ -1,7 +1,7 @@
 nohup python3 cnn.py --data_dir=/home/liuzhenfu/training_data/positive_data/ \
  --valid_dir=/home/liuzhenfu/training_data/test_data \
- --graph_dir=/mnt/liuzhenfu/graphs/ \
- --checkpoint_dir=/mnt/liuzhenfu/checkpoints/ \
+ --graph_dir=/mnt/liuzhenfu/graphs_new/ \
+ --checkpoint_dir=/mnt/liuzhenfu/checkpoints_new/ \
  --charater_num=8877 \
  --batch_size=400 \
  --read_from_checkpoint=True \
--- valid_false.sh
+++ valid_false.sh
@@ -1,7 +1,7 @@
 nohup python3 cnn.py --data_dir=/home/liuzhenfu/training_data/positive_data/ \
  --valid_dir=/home/liuzhenfu/training_data/negative_data \
- --graph_dir=/mnt/liuzhenfu/graphs/ \
- --checkpoint_dir=/mnt/liuzhenfu/checkpoints/ \
+ --graph_dir=/mnt/liuzhenfu/graphs_new/ \
+ --checkpoint_dir=/mnt/liuzhenfu/checkpoints_new/ \
  --charater_num=8877 \
  --batch_size=386 \
  --read_from_checkpoint=True \
