000d0f545866bd10fb9b4ae9981f8b9a0449e6e0
croath <croathliu@gmail.com>
2017-11-14 18:34:20 +0800

    dump and server script

--- dump_ttf.sh
+++ dump_ttf.sh
@@ -2,5 +2,7 @@
 
 OP=/Users/croath/Documents/output/
 mkdir -p "$OP"
-python3 dump_ttf.py --output="$OP"
-find "$OP" -name "*.png" | xargs -I{} convert {} -resize 64x64 +antialias -gravity center -extent 64x64 {}
+NAME=`basename $1`
+mkdir -p "$OP""$NAME"
+python3 dump_ttf.py --output="$OP""$NAME" --fontfile="$1"
+find "$OP"/"$NAME" -name "*.png" | xargs -I{} convert {} -resize 64x64 +antialias -gravity center -extent 64x64 {}
--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
-find $1 -type f -name ".*" -delete
-find $1 -name "*.png" | xargs -I{} convert {} -resize 64x64 +antialias -gravity center -extent 64x64 {}
+# find $1 -type f -name ".*" -delete
+# find $1 -name "*.png" | xargs -I{} convert {} -resize 64x64 +antialias -gravity center -extent 64x64 {}
 
-python3 server.py --model_path=/mnt/liuzhenfu/models_new/model.pb \
- --labellist=/home/liuzhenfu/han/labels.list \
+python3 server.py --model_path=/Users/croath/Documents/model_opti.pb \
+ --labellist=/Users/croath/Documents/labels.list \
  --test_dir=$1
